6a5f83af95c97cc838d7d1c0760e5426c1da55a8
David Plass <dplassgit@users.noreply.github.com>
2021-06-14 19:34:03 +0000

    Just spins around the edge

--- server.py
+++ server.py
@@ -7,7 +7,9 @@ import cherrypy
 This is a simple Battlesnake server written in Python.
 For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
 """
-
+possible_moves = ["up", "down", "left", "right"]
+moves_dx = [0, 0, -1, 1]
+moves_dy = [1, -1, 0, 0]
 
 class Battlesnake(object):
     @cherrypy.expose
@@ -28,14 +30,16 @@ class Battlesnake(object):
     @cherrypy.tools.json_in()
     def start(self):
         # This function is called everytime your snake is entered into a game.
-        
+        self.last_move = -1
         print("START")
         return "ok"
 
-    def print_board(self, data):
+    def make_board(self, data):
       board = [['.' for x in range(self.width)] for y in range(self.height)]
+      self.foods = []
       for food in data["board"]["food"]:
         board[food["y"]][food["x"]] = "F"
+        self.foods.append((food["y"], food["x"]))
       for snake in data["board"]["snakes"]:
         body = snake["body"]
         for segment in body:
@@ -47,9 +51,21 @@ class Battlesnake(object):
         board[segment["y"]][segment["x"]] = "m"
       head = you["head"]
       board[head["y"]][head["x"]] = "M"
-      for row in range(self.height-1, 0, -1):
-        print(''.join(board[row]))
+      return board
   
+    def good_move(self, try_move_idx):
+      good = True
+      dx = moves_dx[try_move_idx]
+      dy = moves_dy[try_move_idx]
+      good = good and (self.y+dy >= 0)
+      good = good and (self.x+dx >= 0)
+      good = good and (self.y+dy < self.height)
+      good = good and (self.x+dx < self.width)
+      if good:
+        good = self.board[self.y+dy][self.x+dx] in ('.', 'F')
+
+      return good
+
     @cherrypy.expose
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
@@ -59,16 +75,31 @@ class Battlesnake(object):
         self.width = data["board"]["width"]
         self.height = data["board"]["height"]
         self.me = data["you"]
+        self.health  = self.me["health"]
         self.x = self.me["head"]["x"]
         self.y = self.me["head"]["y"]
         print("x,y=%d,%d" % (self.x, self.y))
 
-        self.print_board(data)
+        self.board = self.make_board(data)
+        #for row in range(self.height, 0, -1):
+        #  print(''.join(self.board[row]))
 
-        # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
+        # Pick a direction to move in, unless it's bad
+        if self.last_move == -1:
+          self.last_move = 0
 
+        move = ''
+        if self.good_move(self.last_move):
+          move = possible_moves[self.last_move]
+        else:
+          for i in range(0, 4):
+            if self.good_move(i):
+              move = possible_moves[i]
+              self.last_move = i
+              break
+        if move == '':
+          print("Could not find a good move!")
+          move = "up"
         print(f"MOVE: {move}")
         return {"move": move}
 
